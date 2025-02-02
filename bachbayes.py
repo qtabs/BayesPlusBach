7576d4fce30f78dcf8ba82cfa7453ef3de19b9a5
qtabas@gmail.com <qtabas@gmail.com>
2025-02-02 23:06:50 +0100

    Added optimal markov benchmark

--- bachbayes.py
+++ bachbayes.py
@@ -10,7 +10,7 @@ from tqdm import tqdm
 # Neural network models
 class RNN(nn.Module):
 
-	def __init__(self, n_dim, n_layers, n_hidden, unit_type, batch_size=None):
+	def __init__(self, n_dim, n_layers, n_hidden, unit_type, dev, batch_size=None):
 		
 		super(RNN, self).__init__()
 
@@ -18,20 +18,21 @@ class RNN(nn.Module):
 		self.n_layers  = n_layers
 		self.n_hidden  = n_hidden
 		self.unit_type = unit_type
+		self.dev       = dev
 
 		if self.unit_type.lower() == 'elman':
-			self.rnn = nn.RNN(n_dim, n_hidden, n_layers, batch_first=True)
+			self.rnn = nn.RNN(n_dim, n_hidden, n_layers, batch_first=True).to(self.dev)
 		elif self.unit_type.lower() == 'gru':
-			self.rnn = nn.GRU(n_dim, n_hidden, n_layers, batch_first=True)
+			self.rnn = nn.GRU(n_dim, n_hidden, n_layers, batch_first=True).to(self.dev)
 		elif self.unit_type.lower() == 'lstm':
-			self.rnn = nn.LSTM(n_dim, n_hidden, n_layers, batch_first=True)
+			self.rnn = nn.LSTM(n_dim, n_hidden, n_layers, batch_first=True).to(self.dev)
 		else:
-			print('Unit type {self.unit_type} not recognised')
+			print(f'Unit type {self.unit_type} not recognised')
 
-		self.out_obs  = nn.Linear(n_hidden, n_dim, bias=True)
-		self.sig_obs  = nn.Sigmoid()
-		self.out_pred = nn.Linear(n_hidden, n_dim, bias=True)
-		self.sig_pred = nn.Sigmoid()
+		self.out_obs  = nn.Linear(n_hidden, n_dim, bias=True).to(self.dev)
+		self.sig_obs  = nn.Sigmoid().to(self.dev)
+		self.out_pred = nn.Linear(n_hidden, n_dim, bias=True).to(self.dev)
+		self.sig_pred = nn.Sigmoid().to(self.dev)
 
 		if batch_size is not None:
 			self.init_hidden(batch_size)
@@ -40,7 +41,7 @@ class RNN(nn.Module):
 
 	def init_hidden(self, batch_size):
 		h_size = [self.n_layers, batch_size, self.n_hidden]
-		self.hidden = torch.zeros(*h_size)
+		self.hidden = torch.zeros(*h_size).to(self.dev)
 
 	def forward(self, x):
 
@@ -54,13 +55,14 @@ class RNN(nn.Module):
 
 class FeedForwardNN(nn.Module):
 
-	def __init__(self, n_dim, n_hidden_layers, n_hidden_units):
+	def __init__(self, n_dim, n_hidden_layers, n_hidden_units, dev):
 		
 		super(FeedForwardNN, self).__init__()
 
 		self.n_dim           = n_dim
 		self.n_hidden_layers = n_hidden_layers
 		self.n_hidden_units  = n_hidden_units
+		self.dev             = dev
 
 		layers = [nn.Linear(self.n_dim, self.n_hidden_units), nn.ReLU()]
 		for _ in range(self.n_hidden_layers):
@@ -68,18 +70,22 @@ class FeedForwardNN(nn.Module):
 			layers.append(nn.ReLU())
 		layers += [nn.Linear(self.n_hidden_units, self.n_dim), nn.Sigmoid()]
 
-		self.feedforward = nn.Sequential(*layers)
+		self.feedforward = nn.Sequential(*layers).to(self.dev)
 
 	def forward(self, x):
-		x = self.feedforward(x)  # value OR probability???????
-		return x
+		x = self.feedforward(x) 
+		# x serves as either inferred value or prediction:
+		return x, x 
 
 
 # Data Loading
 class Chunker():
 
-	def __init__(self, songs_path, batch_size, chunk_size, chromatic, noise, dev):
+	def __init__(self, songs_path, batch_size, chunk_size, chromatic, noise, dev=None):
 		
+		if dev is None:
+			dev = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 		self.songs_path = songs_path
 		self.batch_size = batch_size
 		self.chunk_size = chunk_size
@@ -176,45 +182,50 @@ class Bachmodel():
 
 		self.pars = pars
 		self.dev  = dev
-		self.chromatic        = pars['chromatic']
-		self.noise            = pars['noise']
-		self.train_path       = os.path.join(pars['datapath'] , 'train')
-		self.validation_path  = os.path.join(pars['datapath'] , 'test')
-		self.test_path        = os.path.join(pars['datapath'] , 'test')
+		self.chromatic       = pars['chromatic']
+		self.noise           = pars['noise']
+		self.train_path      = os.path.join(pars['datapath'], 'train')
+		self.validation_path = os.path.join(pars['datapath'], 'test')
+		self.test_path       = os.path.join(pars['datapath'], 'test')
 
 		n_dim = 12 if self.chromatic else 108
-		n_layers  = pars['n_layers']
-		n_hidden  = pars['n_hidden']
-		unit_type = pars['unit_type']
+		n_layers = pars['n_layers']
+		n_hidden = pars['n_hidden']
+		mod_type = pars['mod_type']
 
-		self.rnn = RNN(n_dim, n_layers, n_hidden, unit_type).to(dev)
+		if mod_type.lower() == 'feedforward':
+			self.net = FeedForwardNN(n_dim, n_layers, n_hidden, dev=dev)
+		else:
+			self.net = RNN(n_dim, n_layers, n_hidden, mod_type, dev=dev)
 
-		modname =  f'{unit_type}_nhidden-{n_hidden:d}_nlayers-{n_layers:d}'
+		modname =  f'{mod_type}_nhidden-{n_hidden:d}_nlayers-{n_layers:d}'
 		modname += f'{"_chromatic" if self.chromatic else ""}'
 		modname += f'_noise{self.noise}'.replace('.', 'p')
 		self.modname = modname
 		self.modpath = f'./models/{modname}.pth'
 
 	def load_weights(self, suffix=''):
-		modpath  = self.modpath.split('.')
-		loadpath = modpath[0] + suffix + modpath[1]
-		self.rnn.load_state_dict(torch.load(loadpath))
+		weightpath = self._compose_weightpath_(suffix)
+		self.net.load_state_dict(torch.load(weightpath))
 
 	def save_weights(self, suffix=''):
-		modpath = self.modpath.split('.')
-		savepath = modpath[0] + suffix + modpath[1]
-		torch.save(self.rnn.state_dict(), self.modpath)
+		weightpath = self._compose_weightpath_(suffix)
+		torch.save(self.net.state_dict(), weightpath)
 
-	def train(self, lr, chunk_size, batch_size, n_batches, freeze=[], obj=['obs']):
+	def trained_weights_exists(self, suffix=''):
+		weightpath = self._compose_weightpath_(suffix)
+		return os.path.exists(weightpath)
+
+	def train(self, lr, chunk_size, batch_size, n_batches, freeze=[], obj=['obs'], target_as_input=False):
 		
 		tolerance = 0.5 # loss < validation_loss + tol * std_validation_loss --> early stopping 
 		chunker = Chunker(self.train_path,batch_size,chunk_size,self.chromatic,self.noise,self.dev)
   
 		# Select which parameters to train
 		parameters_to_train = []
-		for name, parameter in self.rnn.named_parameters():
+		for name, parameter in self.net.named_parameters():
 			if name.split('.')[0] in freeze:
-				print(f'Freezing {name}')
+				# print(f'Freezing {name}')
 				parameter.requires_grad = False
 			else:
 				parameter.requires_grad = True
@@ -229,7 +240,7 @@ class Bachmodel():
 			optimizer.zero_grad()
 			target, sample = chunker.generate_chunk()
 
-			obs, pred = self.rnn(sample)
+			obs, pred = self.net(target if target_as_input else sample)
 
 			loss = 0
 			if 'obs' in obj:
@@ -242,9 +253,9 @@ class Bachmodel():
 
 			loss_hist += [float(loss.detach().cpu().numpy())]
 
-			if batch % 25 == 0:
+			if batch % 100 == 0:
 
-				print(f"Batch {batch:04}/{n_batches} | Loss: {loss:.4f}")
+				# print(f"Batch {batch:04}/{n_batches} | Loss: {loss:.4f}")
 				
 				# Early stopping if we begin to overfit the data when training RNNs
 				if 'rnn' not in freeze and batch > 0.05 * n_batches:
@@ -276,7 +287,7 @@ class Bachmodel():
 			
 			for n_sample in (tqdm(range(n_samples)) if n_samples > 20  else range(n_samples)):	
 				target, sample = chunker.generate_chunk()
-				obs, pred = self.rnn(sample)
+				obs, pred = self.net(sample)
 
 				loss = 0
 				if 'obs' in obj:
@@ -292,10 +303,59 @@ class Bachmodel():
 
 		report_name = self.modname
 		report_name += f'_obj-{"-".join(obj)}_'
-		report_name += f'freeze-{"none" if freeze is None else "-".join(freeze)}'
+		report_name += f'freeze-{"none" if freeze == [] else "-".join(freeze)}'
 		report_path = os.path.join(os.path.split(self.modpath)[0], report_name) + '.txt'
 		history_str = ','.join([f'{l:.4f}' for l in loss_hist])
 
 		with open(report_path, 'w') as f:
 			f.write(history_str)
+
+	def _compose_weightpath_(self, suffix):
+
+		folder, fname = os.path.split(self.modpath)
+		name, extension = '.'.join(fname.split('.')[:-1]), fname.split('.')[-1]
+		filepath = os.path.join(folder, name + suffix + extension)
+
+		return filepath
+
+
+def test_globaldist_model(pars, n_train_samples, n_test_samples, dev=None):
+
+	if dev is None:
+		dev = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+	if n_train_samples > 512:
+		n_batches  = int(n_train_samples/512)
+		batch_size = 512
+	else:
+		n_batches = 1
+		batch_size = n_train_samples
+
+	train_chunker = Chunker(os.path.join(pars['datapath'], 'train'),
+							batch_size,
+							pars['chunk_size'],
+							pars['chromatic'], 
+							pars['noise'],
+							dev)
+
+	test_chunker  = Chunker(os.path.join(pars['datapath'], 'test'), 
+							n_test_samples, 
+							pars['chunk_size'],
+							pars['chromatic'],
+							pars['noise'],
+							dev)
+
+	glob_dist = torch.zeros(12 if pars['chromatic'] else 108).to(dev)
+	for n in range(n_batches):
+		glob_dist += train_chunker.generate_chunk()[1].mean((0, 1))
+	glob_dist *= 1/glob_dist.sum()
+
+	targets     = test_chunker.generate_chunk()[0]
+	loss_func   = nn.BCELoss(reduction='none')
+	performance = loss_func(glob_dist.expand(targets.shape), targets)
+
+	return performance
+
+
+
 
--- pipeline.py
+++ pipeline.py
@@ -1,8 +1,13 @@
 import bachbayes
+import matplotlib.pyplot as plt
 import numpy as np
 import pickle
+import os
+import seaborn as sns
+import time
 
-def run_pipeline(noise, n_hidden):
+
+def main_fitting(noise, n_hidden):
 
 	pars = {'datapath'   : '.',
 			'chunk_size' : 512,
@@ -10,7 +15,7 @@ def run_pipeline(noise, n_hidden):
 			'noise'      : noise,
 			'n_layers'   : 1,
 			'n_hidden'   : n_hidden,
-			'unit_type'  : 'gru'}
+			'mod_type'   : 'gru'}
 
 	m = bachbayes.Bachmodel(pars)
 
@@ -18,50 +23,246 @@ def run_pipeline(noise, n_hidden):
 	lr = 0.02
 	chunk_size = 512
 	batch_size = 512
-	max_batches_obs = 5000
-	n_batches_pred  = 100
+	max_batches_obs = 1000
+	n_batches_pred  = 500
 	n_test_samples  = 4
 
 	## Training RNN on observation accuracy
+	print('Training on observations...', end='')
+	t0 = time.time()
 	m.train(lr, chunk_size, batch_size, max_batches_obs, freeze=['out_pred'], obj=['obs'])
 	m.save_weights('_observation')
+	print(f' done! Time = {(time.time() - t0)/60:.1f} minutes')
 
 	## Training linear readout on prediction accuracy
+	print('Training on predictions...', end='')
+	t0 = time.time()
 	m.train(lr, chunk_size, batch_size, n_batches_pred, freeze=['rnn', 'out_obs'], obj=['pred'])
 	m.save_weights('_prediction')
+	print(f' done! Time = {(time.time() - t0)/60:.1f} minutes')
 
 	# Testing
-	obs_error = m.test(chunk_size, batch_size, n_test_samples, obj=['obs'])
+	obs_error  = m.test(chunk_size, batch_size, n_test_samples, obj=['obs'])
 	pred_error = m.test(chunk_size, batch_size, n_test_samples, obj=['pred'])
 
 	obs_err_m,  obs_err_e  = obs_error.mean(),  obs_error.std()  #/ n_test_samples**.5
 	pred_err_m, pred_err_e = pred_error.mean(), pred_error.std() #/ n_test_samples**.5
 
-	print(f'Observation error = {obs_err_m:.2f}' + u'\u00B1' + f'{obs_err_e:.2f}')
-	print(f'Prediction error = {pred_err_m:.2f}' + u'\u00B1' + f'{pred_err_e:.2f}')
+	print('\n---------------------------------')
+	print(f'| Observation error = {obs_err_m:.2f}' + u'\u00B1' + f'{obs_err_e:.2f} |')
+	print(f'| Prediction error  = {pred_err_m:.2f}' + u'\u00B1' + f'{pred_err_e:.2f} |')
+	print('---------------------------------\n')
 
-	return(obs_err_m, obs_err_e, pred_err_m, pred_err_e)
+	performance = {'obs_err_m':  obs_err_m,
+				   'obs_err_e':  obs_err_e,
+				   'pred_err_m': pred_err_m,
+				   'pred_err_e': pred_err_e}
+
+	return performance
 
 
-noise_vals    = [0.01] + np.arange(0.1, 2.1, 0.1)
-n_hidden_vals = [2**n for n in range(1, 9)]
+def benchmarking(noise, n_hidden):
 
-obs_m,obs_e,pred_m,pred_e = [np.nan*np.ones((len(noise_vals),len(n_hidden_vals))) for _ in range(4)]
+	# Global parameters
+	lr = 0.02
+	chunk_size = 512
+	batch_size = 512
+	max_batches_rnn = 5000
+	max_batches_ff  = 5000
+	n_test_samples  = 4
 
-for i, noise in enumerate(noise_vals):
-	for j, n_hidden in enumerate(n_hidden_vals):
-		print(f'\n#######################################')
-		print(f'## Training model ({i:02},{j:02}) of ({len(noise_vals)},{len(n_hidden_vals)})) ##')
-		print(f'#######################################\n')
-		obs_m[i,j], obs_e[i,j], pred_m[i,j], pred_e[i,j] = run_pipeline(noise, n_hidden)
+	base_pars = {'datapath'   : '.',
+				 'chunk_size' : chunk_size,
+				 'chromatic'  : True,
+				 'noise'      : noise,
+				 'n_hidden'   : n_hidden}
 
-savedict = {'obs_m':    obs_m,
-			'obs_e':    obs_e,
-			'pred_m':   pred_m,
-			'pred_e':   pred_e,
-			'noise':    noise,
-			'n_hidden': n_hidden}
+	# Observations high-bound -> NN one-step
+	print('Benchmarking (observations high bound)...', end='')
+	t0 = time.time()
+	pars = {'mod_type': 'feedforward', 'n_layers': 3}
+	pars.update(base_pars)
+	m = bachbayes.Bachmodel(pars)
+	if not m.trained_weights_exists('_observation-high'):
+		m.train(lr, chunk_size, batch_size, max_batches_ff, obj=['obs'])
+		m.save_weights('_observation-high')
+	obs_high = m.test(chunk_size, batch_size, n_test_samples, obj=['obs'])
+	print(f' done! Time = {(time.time() - t0)/60:.1f} minutes')
 
-with open('./models/results.picke', 'wb') as f:
-	pickle.dump(savedict, f)
+	# Predictions high-bound: global distribution
+	print('Benchmarking (predictions high bound)...', end='')
+	t0 = time.time()
+	n_train = max(max_batches_rnn, max_batches_ff) * batch_size
+	n_test  = n_test_samples * batch_size
+	pred_high = bachbayes.test_globaldist_model(base_pars, n_train, n_test)
+	print(f' done! Time = {(time.time() - t0)/60:.1f} minutes')
+
+	# Predictions markov high bound: 1-back predictions
+	print('Benchmarking (predictions markov bound)...', end='')
+	t0 = time.time()
+	pars = {'mod_type': 'feedforward', 'n_layers': 3}
+	pars.update(base_pars)
+	m = bachbayes.Bachmodel(pars)
+	if not m.trained_weights_exists('_prediction-markov-high'):
+		m.train(lr, chunk_size, batch_size, max_batches_ff, obj=['pred'])
+		m.save_weights('_prediction-markov-high')
+	pred_markh = m.test(chunk_size, batch_size, n_test_samples, obj=['pred'])
+	print(f' done! Time = {(time.time() - t0)/60:.1f} minutes')
+
+	# Predictions markov low bound: 1-back predictions with ground-truth state
+	print('Benchmarking (predictions markov low bound)...', end='')
+	t0 = time.time()
+	pars = {'mod_type': 'feedforward', 'n_layers': 3}
+	pars.update(base_pars)
+	m = bachbayes.Bachmodel(pars)
+	if not m.trained_weights_exists('_prediction-markov-low'):
+		m.train(lr, chunk_size, batch_size, max_batches_ff, obj=['pred'], target_as_input=True)
+		m.save_weights('_prediction-markov-low')
+	pred_markl = m.test(chunk_size, batch_size, n_test_samples, obj=['pred'])
+	print(f' done! Time = {(time.time() - t0)/60:.1f} minutes')
+
+	# Predictions low bound 
+	print('Benchmarking (predictions low bound)...', end='')
+	t0 = time.time()
+	pars = {'mod_type': 'gru', 'n_layers': 1}
+	pars.update(base_pars)
+	m = bachbayes.Bachmodel(pars)
+	if not m.trained_weights_exists('_prediction-low'):
+		m.train(lr, chunk_size, batch_size, max_batches_rnn, obj=['pred'])
+		m.save_weights('_prediction-low')
+	pred_low = m.test(chunk_size, batch_size, n_test_samples, obj=['pred'])
+	print(f' done! Time = {(time.time() - t0)/60:.1f} minutes')
+
+	obs_high_m,  obs_high_e    = obs_high.mean(),   obs_high.std()
+	pred_high_m, pred_high_e   = pred_high.mean(),  pred_high.std()
+	pred_markh_m, pred_markh_e = pred_markh.mean(), pred_markh.std()
+	pred_markl_m, pred_markl_e = pred_markl.mean(), pred_markl.std()
+	pred_low_m,  pred_low_e    = pred_low.mean(),   pred_low.std()
+
+	print('\n----------------------------------------')
+	print(f'| Observation high-bound   = {obs_high_m:.2f}'   + u'\u00B1' + f'{obs_high_e:.2f} |')
+	print(f'| Predictions high-bound   = {pred_high_m:.2f}'  + u'\u00B1' + f'{pred_high_m:.2f} |')
+	print(f'| Predictions markov-bound = {pred_markl_m:.2f}' + u'\u00B1' + f'{pred_markl_e:.2f} |')
+	print(f'| Predictions low-bound    = {pred_low_m:.2f}'   + u'\u00B1' + f'{pred_low_e:.2f} |')
+	print('----------------------------------------\n')
+
+	benchmarks = {'obs_high_m':   obs_high_m,
+				  'obs_high_e':   obs_high_e,
+				  'pred_high_m':  pred_high_m,
+				  'pred_high_e':  pred_high_e,
+				  'pred_markh_m': pred_markh_m,
+				  'pred_markh_e': pred_markh_e,
+				  'pred_markl_m': pred_markl_m,
+				  'pred_markl_e': pred_markl_e,
+				  'pred_low_m':   pred_low_m,
+				  'pred_low_e':   pred_low_e}
+
+	return benchmarks
+
+
+def pipeline():
+
+	noise_vals    = [0.001] + [round(0.1 * n, 2) for n in range(1, 21)]
+	n_hidden_vals = [2**n for n in range(1, 9)]
+
+	save_to_results(noise=noise_vals, n_hidden=n_hidden_vals)
+
+	for i, noise in enumerate(noise_vals):
+		for j, n_hidden in enumerate(n_hidden_vals):
+			
+			print(f'\nTraining model ({i:02},{j:02}) of ({len(noise_vals)},{len(n_hidden_vals)}))')
+			print('--------------------------------')
+			t0 = time.time()
+			
+			savedict = load_results()
+
+			if 'obs_m' not in savedict or np.isnan(savedict['obs_m'][i, j]):
+				performance = main_fitting(noise, n_hidden)
+				save_to_results(noise, n_hidden, **performance)
+
+			if 'pred_high_m' not in savedict or np.isnan(savedict['pred_high_m'][i, j]):
+				benchmarks = benchmarking(noise, n_hidden)
+				save_to_results(noise, n_hidden, **benchmarks)
+
+			print(f'------ model took {(time.time() - t0)/60:<5.1f}minutes ------')
+
+def save_to_results(this_noise=None, this_n_hidden=None, **kwargs):
+
+	if not os.path.exists('./models/results.pickle'):
+		savedict = {}
+	else:
+		with open('./models/results.pickle', 'rb') as f:
+			savedict = pickle.load(f)
+
+	savedict.update({key: kwargs[key] for key in ['noise', 'n_hidden'] if key in kwargs})
+
+	noise, n_hidden = savedict['noise'], savedict['n_hidden']
+	for key in [k for k in kwargs if k not in savedict]: 
+		savedict[key] = np.nan * np.ones((len(noise), len(n_hidden)))
+
+	if this_noise is not None and this_n_hidden is not None:
+		ix_noise, ix_n = noise.index(this_noise), n_hidden.index(this_n_hidden)
+		for key in [k for k in kwargs if k not in ['noise', 'n_hidden']]:
+			savedict[key][ix_noise, ix_n] = kwargs[key]
+
+	with open('./models/results.pickle', 'wb') as f:
+		pickle.dump(savedict, f)
+
+
+def load_results():
+
+	with open('./models/results.pickle', 'rb') as f:
+		savedict = pickle.load(f)
+
+	return savedict
+
+
+def plot_errors():
+
+	savedict = load_results()
+
+	noise, n_hidden = np.round(savedict['noise'], 2), savedict['n_hidden']
+	obs_m, obs_e, pred_m, pred_e = savedict['obs_m'], savedict['obs_e'], savedict['pred_m'], savedict['pred_e']
+	obs_high_m, obs_high_e   = savedict['obs_high_m'],  savedict['obs_high_e']
+	pred_high_m, pred_high_e = savedict['pred_high_m'], savedict['pred_high_e']
+	pred_mark_m, pred_mark_e = savedict['pred_mark_m'], savedict['pred_mark_e']
+	pred_low_m, pred_low_e   = savedict['pred_low_m'],  savedict['pred_low_e']
+
+	obs_n        = obs_m  - obs_high_m   / (.5*obs_e**2  + .5*obs_high_e**2 )**.5
+	pred_n       = pred_m - pred_high_m  / (.5*pred_e**2 + .5*pred_high_e**2)**.5
+	markov_imp_n = pred_m - pred_mark_m  / (.5*pred_e**2 + .5*pred_mark_e**2)**.5
+
+	d = (pred_m - obs_m) / (.5*obs_e**2 + .5*pred_e**2)**.5
+	D = (pred_m - obs_m)
+
+	fig, axs = plt.subplots(3,1)
+
+	sns.heatmap(obs_n.T,  square=True, xticklabels=noise, yticklabels=n_hidden, 
+				annot=True, fmt='.2f', vmin=-10, vmax=0, ax=axs[0])
+	axs[0].set_ylabel('relative noise')
+	axs[0].set_ylabel('hidden units')
+	axs[0].set_title('d(observation error - high bound)')
+
+	sns.heatmap(pred_n.T, square=True, xticklabels=noise, yticklabels=n_hidden, 
+				annot=True, fmt='.2f', vmin=-0.5, vmax=0, ax=axs[1])
+	axs[1].set_ylabel('relative noise')
+	axs[1].set_ylabel('hidden units')
+	axs[1].set_title('d(prediction error - high bound)')
+
+	sns.heatmap(markov_imp_n.T, square=True, xticklabels=noise, yticklabels=n_hidden, 
+				annot=True, fmt='.2f', vmin=-10, vmax=0, ax=axs[2])
+	axs[2].set_ylabel('relative noise')
+	axs[2].set_ylabel('hidden units')
+	axs[2].set_title('d(prediction error - markovian bound)')
+
+	fig.subplots_adjust(left=0.07, bottom=0.05, right=0.99, top=0.95, hspace=0.2)
+	fig.set_size_inches(12, 16)
+	fig.savefig('errors.png')
+
+
+pipeline()
+plot_errors()
+
+
+
 
